436df584c58fd04107fbcb6da8219e52aefd70a0
thanhthong <thongduong61134@gmail.com>
2022-09-05 19:38:35 +0700

    composition api

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,41 @@
-<template>Overview page is here...</template>
+<template>
+  <h1>Overview page is here...</h1>
+  <input type="text" placeholder="Enter the customer..." v-model="searchText" />
+  <ul>
+    <li v-for="cs in customersFilleed" :key="cs">{{ cs }}</li>
+  </ul>
+</template>
 
 <script>
-export default {};
+import { ref } from "@vue/reactivity";
+import { computed, watch, watchEffect } from "@vue/runtime-core";
+export default {
+  setup() {
+    const searchText = ref("");
+    const customers = ["hehe", "haha"];
+    const customersFilleed = computed(() =>
+      customers
+        .map((cs) => {
+          cs.toLowerCase();
+          return cs;
+        })
+        .filter((cs) => cs.includes(searchText.value.toLowerCase()))
+    );
+
+    watch(searchText, (newValue, oldValue) => {
+      console.log(newValue, oldValue);
+    });
+
+    watchEffect(() => {
+      if (searchText.value) console.log("running...");
+    });
+
+    return {
+      searchText,
+      customersFilleed,
+    };
+  },
+};
 </script>
 
 <style></style>
